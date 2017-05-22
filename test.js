a4d315329752f33a9a5164ddf3ce9ea5a5af4fd6
ravikishorethella <ravikishorethella@gmail.com>
2017-05-21 20:39:10 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,8 @@
+//select all divs and give them a purple background
+$("div").css("background-color", "purple");
+// selct the  class with highlight and make them 200px wide
+$(".highlight").css("width", "200px");
+//select the div with id third and give it a orange border
+$("#third").css("border", "2px solid orange");
+// select the first div only and change its font-color to pink
+$("div:first-of-type").css("color", "pink");
