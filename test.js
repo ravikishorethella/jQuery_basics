525ed8d85412e9e945f8d5b5f71b994717e4696a
ravikishorethella <ravikishorethella@gmail.com>
2017-05-22 20:11:10 -0400

    click and keypress event

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-//select all divs and give them a purple background
-$("div").css("background-color", "purple");
-// selct the  class with highlight and make them 200px wide
-$(".highlight").css("width", "200px");
-//select the div with id third and give it a orange border
-$("#third").css("border", "2px solid orange");
-// select the first div only and change its font-color to pink
-$("div:first-of-type").css("color", "pink");
+////select all divs and give them a purple background
+//$("div").css("background-color", "purple");
+//// selct the  class with highlight and make them 200px wide
+//$(".highlight").css("width", "200px");
+////select the div with id third and give it a orange border
+//$("#third").css("border", "2px solid orange");
+//// select the first div only and change its font-color to pink
+//$("div:first-of-type").css("color", "pink");
+
+//// jQuery click events
+//$("h1").click(function(){
+//    alert("you clikced the heading");
+//    $(this).css("background","pink");
+//});
+
+//// jQuery keypress events
+//$("input").keypress(function(){
+//    alert("key input press!");    
+//});
+
